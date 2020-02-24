ae90744633188cfc19f529a48e34906ee3a6d2b2
Ryan Jones <ryandjones14@gmail.com>
2020-02-23 22:59:29 -0600

    Revert "create router, uses router for home and about page"
    This reverts commit b37509c453fee0858e5494890c01a4a6861d76b1.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 var serveStatic = require('serve-static');
 var path = require('path');
 const mongoose = require('mongoose');
@@ -12,10 +13,12 @@ mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
 });
 
+
 const app = express();
 
 const names = require('./models/Name');
 
+app.use(morgan('tiny'));
 app.use(cors());
 app.use(bodyParser.json());
 
